477059bd8cd65576d378cfe06de1d8fa39451112
Zongle Wang <wangzongler@gmail.com>
2024-01-15 23:48:59 +0800

    Bump string-res-exporter to 0.1.1 (#3)

--- Formula/string-res-exporter.rb
+++ Formula/string-res-exporter.rb
@@ -1,9 +1,9 @@
 class StringResExporter < Formula
   desc "A tool for exporting and importing string resources from an Android project to a XLS file"
   homepage "https://github.com/Goooler/StringResExporter"
-  url "https://github.com/Goooler/StringResExporter/releases/download/v0.1.0/string-res-exporter-0.1.0-binary.jar"
-  version "0.1.0"
-  sha256 "4a0991cff81bab2fee72f0347035e2d419c73fe3c606ae804702db0ab4b629a7"
+  url "https://github.com/Goooler/StringResExporter/releases/download/v0.1.1/string-res-exporter-0.1.1-binary.jar"
+  version "0.1.1"
+  sha256 "47af088db59847762dfdb17443186ba5946c95f339f610f5b1b8325520f1c096"
 
   depends_on "openjdk"
 
