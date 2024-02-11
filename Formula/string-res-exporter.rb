3f15b1b9c4c394c7bbca34151cfc80ab0cf3d95a
Goooler <wangzongler@gmail.com>
2024-02-11 18:48:47 +0800

    Fix

--- Formula/string-res-exporter.rb
+++ Formula/string-res-exporter.rb
@@ -2,7 +2,7 @@ class StringResExporter < Formula
   desc "A tool for exporting and importing string resources from an Android project to a XLS file"
   homepage "https://github.com/Goooler/StringResExporter"
   url "https://github.com/Goooler/StringResExporter/releases/download/v0.2.1/string-res-exporter-0.2.1-binary.jar"
-  version "0.2.0"
+  version "0.2.1"
   sha256 "3ef13f0d454f73b037c4bfa52bc6814fb92ab815cdaf66d2d8920c6e9728737c"
 
   depends_on "openjdk"
