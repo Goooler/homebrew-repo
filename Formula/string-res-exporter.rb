f144b4caf5d078ff1992068c3a286f3dec00a1b7
Goooler <wangzongler@gmail.com>
2024-02-15 13:54:09 +0800

    Revert version placeholder in url
    Refs https://github.com/Homebrew/actions/issues/487#issuecomment-1945414611.

--- Formula/string-res-exporter.rb
+++ Formula/string-res-exporter.rb
@@ -2,7 +2,7 @@ class StringResExporter < Formula
   desc "A tool for exporting and importing string resources from an Android project to a XLS file"
   homepage "https://github.com/Goooler/StringResExporter"
   version "0.2.1"
-  url "https://github.com/Goooler/StringResExporter/releases/download/v#{version}/string-res-exporter-#{version}-binary.jar"
+  url "https://github.com/Goooler/StringResExporter/releases/download/v0.2.1/string-res-exporter-0.2.1-binary.jar"
   sha256 "3ef13f0d454f73b037c4bfa52bc6814fb92ab815cdaf66d2d8920c6e9728737c"
 
   depends_on "openjdk"
