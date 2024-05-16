7df4848385fa115a565638ab8488f19015fb7fba
Zongle Wang <wangzongler@gmail.com>
2024-05-16 16:26:32 +0800

    kotlin-explorer 1.1.0

--- Casks/kotlin-explorer.rb
+++ Casks/kotlin-explorer.rb
@@ -1,9 +1,8 @@
 cask "kotlin-explorer" do
-  version "1.0.1"
-  sha256 "52a0c0612eaca020e28ff59f944aa16961367e95c6f5fa99f36d7a1bd45c071a"
+  version "1.1.0"
+  sha256 "51820eee6d61ad6179f3ad16db86544799e179d9314e52b82c7ae70ee13b1457"
 
-  # TODO: the path should be updated to "romainguy/kotlin-explorer/releases/download/v#{version}/kotlin-explorer-#{version}.dmg"
-  url "https://github.com/romainguy/kotlin-explorer/releases/download/v1.0.0/kotlin-explorer-#{version}.dmg"
+  url "https://github.com/romainguy/kotlin-explorer/releases/download/v#{version}/kotlin-explorer-#{version}.dmg"
   name "Kotlin Explorer"
   desc "Kotlin Explorer is a desktop tool to quickly and easily disassemble Kotlin code into: Java bytecode, Android DEX bytecode, and Android OAT assembly."
   homepage "https://github.com/romainguy/kotlin-explorer"
