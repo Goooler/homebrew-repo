6072e197f29c3152fa90de885f8b42ea9e856117
Zongle Wang <wangzongler@gmail.com>
2024-05-22 09:50:46 +0800

    kotlin-explorer 1.2.2 (#11)
    Co-authored-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/kotlin-explorer.rb
+++ Casks/kotlin-explorer.rb
@@ -1,10 +1,10 @@
 cask "kotlin-explorer" do
-  version "1.2.0"
-  sha256 "5af955fa83874f254cede4593833b02ae3e32bb96e3115c2a1029893130ba667"
+  version "1.2.2"
+  sha256 "2781f1718cb7d0f317c50f8ae6f617ef73cf5d827d45499e94277d91f7c1e4e6"
 
   url "https://github.com/romainguy/kotlin-explorer/releases/download/v#{version}/Kotlin.Explorer-#{version}.dmg"
   name "Kotlin Explorer"
-  desc "Kotlin Explorer is a desktop tool to quickly and easily disassemble Kotlin code."
+  desc "Kotlin Explorer is a desktop tool to quickly and easily disassemble Kotlin code"
   homepage "https://github.com/romainguy/kotlin-explorer"
 
   auto_updates true
