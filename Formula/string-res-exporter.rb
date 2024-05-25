a215ab1468a20d408fad1838ba8f169f1c58827d
Goooler <wangzongler@gmail.com>
2024-05-25 19:06:19 +0800

    Revert "Update string-res-exporter.rb format"
    This reverts commit 92bc36eb

--- Formula/string-res-exporter.rb
+++ Formula/string-res-exporter.rb
@@ -4,12 +4,12 @@ class StringResExporter < Formula
 
   desc "A tool for exporting and importing string resources from an Android project to a XLS file"
   homepage "https://github.com/Goooler/StringResExporter"
-  url "https://github.com/Goooler/StringResExporter/releases/download/v#{version}/string-res-exporter-#{version}-binary.jar"
+  url "https://github.com/Goooler/StringResExporter/releases/download/v0.2.3/string-res-exporter-0.2.3-binary.jar"
 
   depends_on "openjdk"
 
   def install
-    mv "string-res-exporter-#{version}-binary.jar", "string-res-exporter.jar"
+    mv "string-res-exporter-0.2.3-binary.jar", "string-res-exporter.jar"
     libexec.install "string-res-exporter.jar"
     bin.write_jar_script libexec / "string-res-exporter.jar", "string-res-exporter"
   end
