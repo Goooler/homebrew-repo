1f87dd97fb4a06ff52ece0b59d982a7406498412
Zongle Wang <wangzongler@gmail.com>
2024-05-26 08:42:27 +0800

    Add audit workflow to check all packages (#12)

--- Formula/string-res-exporter.rb
+++ Formula/string-res-exporter.rb
@@ -1,10 +1,8 @@
 class StringResExporter < Formula
-  version "0.2.3"
-  sha256 "54349e3196b3e5bf7a86de2a8e4b9e89ec29849d4d82a2a9b9da3e53572c1480"
-
   desc "A tool for exporting and importing string resources from an Android project to a XLS file"
   homepage "https://github.com/Goooler/StringResExporter"
   url "https://github.com/Goooler/StringResExporter/releases/download/v0.2.3/string-res-exporter-0.2.3-binary.jar"
+  sha256 "54349e3196b3e5bf7a86de2a8e4b9e89ec29849d4d82a2a9b9da3e53572c1480"
 
   depends_on "openjdk"
 
