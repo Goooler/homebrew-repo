16d0e134e19bc51fc935563c3cc25b0bf6ac205f
Goooler <wangzongler@gmail.com>
2024-06-05 09:24:26 +0800

    kotlin-explorer 1.2.3

--- Casks/kotlin-explorer.rb
+++ Casks/kotlin-explorer.rb
@@ -1,10 +1,10 @@
 cask "kotlin-explorer" do
-  version "1.2.2"
-  sha256 "2781f1718cb7d0f317c50f8ae6f617ef73cf5d827d45499e94277d91f7c1e4e6"
+  version "1.2.3"
+  sha256 "5d1b733f24fde502a752987c417376483c496cb02f69c002499337f4bffc25dc"
 
-  url "https://github.com/romainguy/kotlin-explorer/releases/download/v#{version}/Kotlin.Explorer-#{version}.dmg"
+  url "https://github.com/romainguy/kotlin-explorer/releases/download/v#{version}/kotlin-explorer-#{version}.dmg"
   name "Kotlin Explorer"
-  desc "Kotlin Explorer is a desktop tool to quickly and easily disassemble Kotlin code"
+  desc "A desktop tool to quickly and easily disassemble Kotlin code"
   homepage "https://github.com/romainguy/kotlin-explorer"
 
   app "Kotlin Explorer.app"
