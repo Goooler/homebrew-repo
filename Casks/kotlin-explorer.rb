04df2ff0c10dfe5fab1661799cc87d72e19a8661
Zongle Wang <wangzongler@gmail.com>
2024-06-05 09:40:59 +0800

    kotlin-explorer 1.3.0 (#13)
    Co-authored-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/kotlin-explorer.rb
+++ Casks/kotlin-explorer.rb
@@ -1,6 +1,6 @@
 cask "kotlin-explorer" do
-  version "1.2.3"
-  sha256 "5d1b733f24fde502a752987c417376483c496cb02f69c002499337f4bffc25dc"
+  version "1.3.0"
+  sha256 "e5292252bd12c698ef93b184fe2a93aa97f4668e6d52d7a6c4e24902685a963e"
 
   url "https://github.com/romainguy/kotlin-explorer/releases/download/v#{version}/kotlin-explorer-#{version}.dmg"
   name "Kotlin Explorer"
