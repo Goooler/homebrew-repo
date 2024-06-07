b96ef59e5d055c075198731c9fc44ba8741f1486
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-06-07 02:19:54 +0000

    kotlin-explorer 1.4.0

--- Casks/kotlin-explorer.rb
+++ Casks/kotlin-explorer.rb
@@ -1,6 +1,6 @@
 cask "kotlin-explorer" do
-  version "1.3.1"
-  sha256 "e1da70b866766e659f268338a21462fbf04d4e9fc2bb55613eca9a2dac1a4b6b"
+  version "1.4.0"
+  sha256 "18f671a9dedc697de4769fbd6d0f326efe6a6e7d115541fd8e6c8a99218c7368"
 
   url "https://github.com/romainguy/kotlin-explorer/releases/download/v#{version}/kotlin-explorer-#{version}.dmg"
   name "Kotlin Explorer"
