1969d9aa2b377bb40b0e51c315f9f4b936eac589
Goooler <wangzongler@gmail.com>
2024-06-10 16:41:50 +0800

    Remove version

--- Formula/stackzy.rb
+++ Formula/stackzy.rb
@@ -1,14 +1,13 @@
 class Stackzy < Formula
   desc 'A desktop app to analyse APK. Built using Compose desktop'
   homepage 'https://github.com/theapache64/stackzy'
-  version '1.2.6'
 
   if Hardware::CPU.intel?
     sha256 '1bf51366ebd0af9deb9ea2595d455876a4d614ea7a86637bef8812d061c0129f'
-    url "https://github.com/theapache64/stackzy/releases/download/#{version}/Stackzy-macos-x64-#{version}.jar"
+    url "https://github.com/theapache64/stackzy/releases/download/1.2.6/Stackzy-macos-x64-1.2.6.jar"
   else
     sha256 '846ea70de61211becd91c44036c3b20926dd0f94e073eae94ed88b33594a2768'
-    url "https://github.com/theapache64/stackzy/releases/download/#{version}/Stackzy-macos-arm64-#{version}.jar"
+    url "https://github.com/theapache64/stackzy/releases/download/1.2.6/Stackzy-macos-arm64-1.2.6.jar"
   end
 
   def install
