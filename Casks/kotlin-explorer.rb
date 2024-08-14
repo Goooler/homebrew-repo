ced79c508cf1e78bb1bf5d116e47548530a7edc1
Goooler <wangzongler@gmail.com>
2024-08-14 14:29:03 +0800

    Bump kotlin-explorer to 1.4.7 and support Mac x64

--- Casks/kotlin-explorer.rb
+++ Casks/kotlin-explorer.rb
@@ -1,8 +1,14 @@
 cask "kotlin-explorer" do
-  version "1.4.3"
-  sha256 "6001b8f3d25280cc95804e39bd7911b1df01e511db73266757792469804af2a2"
+  version "1.4.7"
 
-  url "https://github.com/romainguy/kotlin-explorer/releases/download/v#{version}/kotlin-explorer-#{version}.dmg"
+  if Hardware::CPU.intel?
+    sha256 '7a1970af3c27936f79077732a9170078f6b010e1acc9fb25082d7501d37801b8'
+    url "https://github.com/romainguy/kotlin-explorer/releases/download/v#{version}/kotlin-explorer-x64-#{version}.dmg"
+  else
+    sha256 '76a152d5d196144f9ff158b16e26c484d44083513713673a681b5fbd1d8d4f32'
+    url "https://github.com/romainguy/kotlin-explorer/releases/download/v#{version}/kotlin-explorer-arm64-#{version}.dmg"
+  end
+
   name "Kotlin Explorer"
   desc "Quickly and easily disassemble Kotlin code"
   homepage "https://github.com/romainguy/kotlin-explorer"
