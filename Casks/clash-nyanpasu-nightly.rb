d5a857164f3c671dc06862b10e90eb94cd516922
Goooler <wangzongler@gmail.com>
2025-01-20 20:03:15 +0800

    Add clash-nyanpasu-nightly.rb

---
+++ Casks/clash-nyanpasu-nightly.rb
@@ -0,0 +1,18 @@
+cask "clash-nyanpasu-nightly" do
+  version "2.0.0-alpha+aaf84fb"
+
+  on_arm do
+    sha256 "bcee837286a58eb786230115d53a0f69448b22cc126621b811cb147f98f03034"
+    url "https://github.com/libnyanpasu/clash-nyanpasu/releases/download/pre-release/Clash.Nyanpasu_#{version}_aarch64.dmg"
+  end
+  on_intel do
+    sha256 "a4d4ff4e9fbe7ab1a73e151451f0b8b599dd97702e9d5706bb08c2d962bceeac"
+    url "https://github.com/libnyanpasu/clash-nyanpasu/releases/download/pre-release/Clash.Nyanpasu_#{version}_x64.dmg"
+  end
+
+  name "Clash Nyanpasu Nightly"
+  desc "Clash GUI based on Tauri"
+  homepage "https://github.com/libnyanpasu/clash-nyanpasu"
+
+  app "Clash Nyanpasu Nightly.app"
+end
