9891d11ed2b964e84a16e4378e30cb8c33ca755b
Goooler <wangzongler@gmail.com>
2025-05-05 00:11:46 +0800

    Bump Hiddify to 2.5.7

--- Casks/hiddify.rb
+++ Casks/hiddify.rb
@@ -1,6 +1,6 @@
 cask "hiddify" do
-  version "2.0.5"
-  sha256 "71d4314ef0ce9d3bffb423a438ebeb9bae9ec8decfe0e17f5e6327201849e138"
+  version "2.5.7"
+  sha256 "9122669659b73e613bda8a7c6ac959d5983faf6739bd8a79e3cf573d20d5ab36"
 
   url "https://github.com/hiddify/hiddify-app/releases/download/v#{version}/Hiddify-MacOS.dmg"
   name "Hiddify"
