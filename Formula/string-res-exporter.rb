8f1d20e79c69e2e7fe7f04e9254a4dc9766a7381
Goooler <wangzongler@gmail.com>
2025-12-21 10:26:56 +0800

    Revert "string-res-exporter 0.2.4" for testing
    This reverts commit d65311956eeeeffcdb674014d3a5d4a822f553cb.

--- Formula/string-res-exporter.rb
+++ Formula/string-res-exporter.rb
@@ -1,13 +1,13 @@
 class StringResExporter < Formula
   desc "A tool for exporting and importing string resources from an Android project to a XLS file"
   homepage "https://github.com/Goooler/StringResExporter"
-  url "https://github.com/Goooler/StringResExporter/releases/download/v0.2.4/string-res-exporter-0.2.4-binary.jar"
-  sha256 "1d8056af4331de5d5d4d7fd30e494779b3f338d77327ec91d7f96b8fcaa7c82a"
+  url "https://github.com/Goooler/StringResExporter/releases/download/v0.2.3/string-res-exporter-0.2.3-binary.jar"
+  sha256 "54349e3196b3e5bf7a86de2a8e4b9e89ec29849d4d82a2a9b9da3e53572c1480"
 
   depends_on "openjdk"
 
   def install
-    mv "string-res-exporter-#{version}-binary.jar", "string-res-exporter.jar"
+    mv "string-res-exporter-0.2.3-binary.jar", "string-res-exporter.jar"
     libexec.install "string-res-exporter.jar"
     bin.write_jar_script libexec / "string-res-exporter.jar", "string-res-exporter"
   end
