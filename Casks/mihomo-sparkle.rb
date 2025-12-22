a59d73978ae84be1e034aa4df69879b72b55be0f
Goooler <wangzongler@gmail.com>
2025-12-22 10:07:23 +0800

    Revert "mihomo-sparkle 1.6.16"
    This reverts commit dac49ea9c005c27c354e39f167e01b19724d808c.

--- Casks/mihomo-sparkle.rb
+++ Casks/mihomo-sparkle.rb
@@ -1,15 +1,15 @@
 cask "mihomo-sparkle" do
-  version "1.6.16"
+  version "1.6.15"
 
   on_arm do
-    sha256 "9d038ee2d802d7c4f2d8008418cca6035a3787df88ca702d8c2668523588e231"
+    sha256 "b2a827dec0c6fe8c38be3067877f845dfa7961b352a6a19971e7cfb1cceb2111"
 
     url "https://github.com/xishang0128/sparkle/releases/download/#{version}/sparkle-macos-#{version}-arm64.pkg"
 
     pkg "sparkle-macos-#{version}-arm64.pkg"
   end
   on_intel do
-    sha256 "1dac90308dfe22b5ec8f60ef61fa9b2966e356e9384aa7788a5789e1d7936698"
+    sha256 "5644cb13e78d6e92ea9c01ff4b1c155ce74409d913a43acdea18508d0399452b"
 
     url "https://github.com/xishang0128/sparkle/releases/download/#{version}/sparkle-macos-#{version}-x64.pkg"
 
