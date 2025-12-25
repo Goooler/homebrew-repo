021bd055760ca771b157687e255702d4fc02d370
Anka <runner@sat12-bq165-0e20a90b-a07e-4312-a89a-c5d0a5590c2f-825438C0BE3E.local>
2025-12-25 08:10:01 +0000

    clash-mac 1.1.24

--- Casks/clash-mac.rb
+++ Casks/clash-mac.rb
@@ -1,15 +1,15 @@
 cask "clash-mac" do
-  version "1.1.22"
+  version "1.1.24"
 
   on_arm do
-    sha256 "a48507ced6d3af387e7b2b49275859b7d0fd370279792da29244bf66a4b6dc9f"
+    sha256 "5e996c2cd498da63bc008af207627c2e620fef8417cca3a34bf992f61850e5e3"
 
     url "https://github.com/666OS/ClashMac/releases/download/v#{version}/ClashMac-v#{version}-macos-arm64.zip"
 
     app "ClashMac-v#{version}-macos-arm64/ClashMac.app"
   end
   on_intel do
-    sha256 "0ea32d20b4134b87a535aa02804efe439173d535e35ce5b6c201a54ca49e3384"
+    sha256 "0c217f9fede29b965a9dcc13f47f18b46dbf82e370e583a3fac734148c5b52f1"
 
     url "https://github.com/666OS/ClashMac/releases/download/v#{version}/ClashMac-v#{version}-macos-x86_64.zip"
 
@@ -20,13 +20,13 @@ cask "clash-mac" do
   desc "Lightweight Clash Menu Bar Client"
   homepage "https://github.com/666OS/ClashMac"
 
+  depends_on macos: ">= :big_sur"
+
   postflight do
     # Informative message for users during installation
     puts "Run `xattr -cr /Applications/ClashMac.app` for the APP, see more details in https://github.com/666OS/ClashMac/blob/main/README.md#solutions."
 
     # TODO: https://github.com/666OS/ClashMac/blob/main/README.md#solutions
-    system_command '/usr/bin/xattr', args: ['-cr', "#{appdir}/ClashMac.app"], sudo: false
+    system_command "/usr/bin/xattr", args: ["-cr", "#{appdir}/ClashMac.app"], sudo: false
   end
-
-  depends_on macos: ">= :big_sur"
 end
