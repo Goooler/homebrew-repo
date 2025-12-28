eda5ec6ff42a6a79fd85bb6ae38235f4632b281c
Goooler <wangzongler@gmail.com>
2025-12-28 16:01:33 +0800

    Revert "Revert cask format optimization - incompatible with auto-bump workflow"
    This reverts commit 022468266bad8b7b6a6f7b54995570ac2ce30194.

--- Casks/clash-nyanpasu.rb
+++ Casks/clash-nyanpasu.rb
@@ -1,15 +1,11 @@
 cask 'clash-nyanpasu' do
+  arch arm: "aarch64", intel: "x64"
+
   version '1.6.1'
+  sha256 arm:   'bcee837286a58eb786230115d53a0f69448b22cc126621b811cb147f98f03034',
+         intel: 'a4d4ff4e9fbe7ab1a73e151451f0b8b599dd97702e9d5706bb08c2d962bceeac'
 
-  on_arm do
-    sha256 'bcee837286a58eb786230115d53a0f69448b22cc126621b811cb147f98f03034'
-    url "https://github.com/libnyanpasu/clash-nyanpasu/releases/download/v#{version}/Clash.Nyanpasu_#{version}_aarch64.dmg"
-  end
-  on_intel do
-    sha256 'a4d4ff4e9fbe7ab1a73e151451f0b8b599dd97702e9d5706bb08c2d962bceeac'
-    url "https://github.com/libnyanpasu/clash-nyanpasu/releases/download/v#{version}/Clash.Nyanpasu_#{version}_x64.dmg"
-  end
-
+  url "https://github.com/libnyanpasu/clash-nyanpasu/releases/download/v#{version}/Clash.Nyanpasu_#{version}_#{arch}.dmg"
   name 'Clash Nyanpasu'
   desc 'Clash GUI based on Tauri'
   homepage 'https://github.com/libnyanpasu/clash-nyanpasu'
