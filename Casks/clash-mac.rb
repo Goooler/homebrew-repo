b04e57fc88de6fe6aa9f55f9da1948e61bfadff6
Goooler <wangzongler@gmail.com>
2025-12-30 10:33:50 +0800

    Fix merge

--- Casks/clash-mac.rb
+++ Casks/clash-mac.rb
@@ -2,8 +2,8 @@ cask "clash-mac" do
   arch arm: "arm64", intel: "x86_64"
 
   version "1.3.2"
-  sha256 arm:   '6d4c73c991e06f1643412d8cf5feee29984d7803e28f9a77d15fcb36c5fb4495',
-         intel: '8b872451cba5104f9879d237c9b1be1b063d2c6430195793be1f90a57a9e7439'
+  sha256 arm:   "6d4c73c991e06f1643412d8cf5feee29984d7803e28f9a77d15fcb36c5fb4495",
+         intel: "8b872451cba5104f9879d237c9b1be1b063d2c6430195793be1f90a57a9e7439"
 
   app "ClashMac-v#{version}-macos-#{arch}/ClashMac.app"
   url "https://github.com/666OS/ClashMac/releases/download/v#{version}/ClashMac-v#{version}-macos-#{arch}.zip"
