f2e0eb4de0fed2abdb1c0bdea83136c6cc84de16
Goooler <wangzongler@gmail.com>
2026-08-27 16:42:50 +0800

    Add Kumone cask

---
+++ Casks/kumone.rb
@@ -0,0 +1,21 @@
+cask "kumone" do
+  version "0.3.0"
+  sha256 "addad1a7c816b0866bdd1449b32fa5ed6ebb4ae3ae3238d0dcd32cd1d4c8d506"
+
+  url "https://github.com/missuo/kumone/releases/download/v#{version}/Kumone-#{version}.zip"
+  name "Kumone"
+  desc "Native client for NetEase Cloud Music"
+  homepage "https://github.com/missuo/kumone"
+
+  depends_on macos: :sequoia
+
+  app "Kumone.app"
+
+  zap trash: [
+    "~/Library/Application Support/Kumone",
+    "~/Library/Caches/im.missuo.Kumone",
+    "~/Library/HTTPStorages/im.missuo.Kumone",
+    "~/Library/Preferences/im.missuo.Kumone.plist",
+    "~/Library/Saved Application State/im.missuo.Kumone.savedState",
+  ]
+end
